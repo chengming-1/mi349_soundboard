fe6ff9751b7860d45c221e42154c9a2d553d4f4a
chengming Wang <wangc115@msu.edu>
2020-11-15 08:00:46 -0500

    first commit

---
+++ soundboard.js
@@ -0,0 +1,11 @@
+function playalarm() {
+    document.getElementById("alarm_beep").play();
+}
+
+function playapplause() {
+    document.getElementById("applause").play();
+}
+
+function playarrow() {
+    document.getElementById("arrow").play();
+}
